ecc3fe0f9679dc0d09356f146dba5b0157bd1100
GauravSuplekar <125793400+GauravSuplekar@users.noreply.github.com>
2026-08-03 15:18:43 +0530

    added entities and  repositories

---
+++ backend/src/main/java/com/global/RecruitmentSystem/repository/ClientRequirementRepository.java
@@ -0,0 +1,7 @@
+package com.global.RecruitmentSystem.repository;
+
+import com.global.RecruitmentSystem.model.ClientRequirement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRequirementRepository extends JpaRepository<ClientRequirement, Integer> {
+}
